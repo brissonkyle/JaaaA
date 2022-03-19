94b41c3d946a3da6af9af3fe85a57590902f7731
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-03-18 23:24:43 -0600

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,79 @@
+var tweets = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+// Used for each function and twits to call on the string and display each iteration
+tweets.forEach(twits);
+function twits(item, _array) {
+    console.log(item);
+}
+
+
+// Attempted bonus :(
+var username = ['1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a']
+var created_at = ['02-03-1999', '02-03-1998', '02-03-1997', '02-03-1996', '02-03-1995', '02-03-1994', '02-03-1993', '02-03-1992', '02-03-1991', '02-03-1990']
+var age = (1)
+
+// 
+var user = [{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+{
+    username: [''],
+    tweets: 'ayooo',
+    age: 1,
+    created_at: [''],
+},
+]
+console.log(user.username);
+console.log(user.tweets['']);
+console.log(user.created_at['']);
